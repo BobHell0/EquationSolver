5ff6cc86fef08bfcfd557f397f72bd108d9f7c28
BobHell0 <102663926+BobHell0@users.noreply.github.com>
2024-11-17 15:19:16 +1100

    made project into a gradle project

---
+++ test/parser/BasicParserTest.java
@@ -0,0 +1,15 @@
+package test.parser;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+public class BasicParserTest {
+
+    
+}
