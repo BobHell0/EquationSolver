ab30bdd41f5419886daaffda6cf7ff53a3ddae05
BobHell0 <102663926+BobHell0@users.noreply.github.com>
2024-11-18 10:06:32 +1100

    created two tests to test for variable addition - printing format and collecting like terms

---
+++ app/src/test/java/org/parser/BasicParsingVariablesTest.java
@@ -0,0 +1,22 @@
+package org.parser;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BasicParsingVariablesTest {
+    @Test
+    @DisplayName("Basic Variable Addition - Non-like terms")
+    public void VariableAdditionNonLikeTerms() {
+        assertEquals(new String("(a+b)"), Parser.parseLine("a + b"));
+    }
+
+    @Test
+    @DisplayName("Basic Variable Addition - Like terms")
+    public void VariableAdditionLikeTerms() {
+        assertEquals(new String ("(2*a)"), Parser.parseLine("a + a"));
+
+    }
+    
+}
